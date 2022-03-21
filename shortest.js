d73b29e68bee6b0ecb358e1313bea75359ccc65c
Benjamin <benjamincook1313@gmail.com>
2022-03-21 17:46:02 -0600

    island count

---
+++ shortest.js
@@ -0,0 +1,45 @@
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges)
+  const visited = new Set()
+  console.log(graph)
+  const queue = [ [nodeA, 0] ]
+
+  while (queue > 0) {
+    const [node, dist] = queue.shift()
+
+    if (node === nodeB) return dist
+
+    for(let neighbor of graph[node]) {
+      if(!visited.has(neighbor))
+      visited.add(neighbor)
+      queue.push([neighbor, dist + 1])
+    }
+  }
+
+  return -1
+};
+
+const buildGraph = (edges) => {
+  const graph = {}
+
+  for(let edge of edges) {
+    const [a, b] = edge
+    if(!(a in graph)) graph[a] = []
+    if(!(b in graph)) graph[b] = []
+    graph[b].push(a)
+    graph[a].push(b)
+  }
+
+  return graph
+};
+
+
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+shortestPath(edges, 'w', 'z')
